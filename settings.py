f4ddedc1367c43dd46aa4e7153ff23896eb17ac4
Matias Iturburu <maturburu@gmail.com>
2011-02-10 16:34:13 -0300

    Saqué permisos de ejecución a los .py Agrego un .gitignore para evitar mocos Agrego un requirements.txt

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'senderonorte',                      # Or path to database file if using sqlite3.
+        'NAME': 'senderonorte.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -94,11 +94,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'senderonorte',
     'excurtion',
     'news',
  )
