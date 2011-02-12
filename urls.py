f2925937277ef26b37f5da82d3a723e0ae767061
“monetti” <onetti.martin@gmail.com>
2011-02-12 09:29:16 -0300

    sendos avances

--- urls.py
+++ urls.py
@@ -4,25 +4,16 @@ from django.views.generic import list_detail
 from django.views.generic.simple import direct_to_template
 from django.conf import settings 
 
-from excurtion.models import Excurtion 
 
 admin.autodiscover()
 
-info = {
-    'template':'index.html',
-}
-
-info_2 = {
-    'queryset':Excurtion.objects.all(),
-    'paginate_by':1,
-    'page':1,
-}
 
 urlpatterns = patterns('',
     (r'^$', 'senderonorte.views.index' , {}, 'home'),
     (r'^acerca_de/', direct_to_template, {'template':'about.html'}),
+    (r'^cimblings/', direct_to_template, {'template':'escalada.html'}),
+    (r'^educationtravel/', direct_to_template, {'template':'educativos.html'}),
 
-    #(r'^home/', list_detail.object_list, info_2),
 )
 
 urlpatterns += patterns( '',
