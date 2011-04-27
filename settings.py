a32d3d3d587fca4ecd246d47641734a9796172f3
tutuca <maturburu@gmail.com>
2011-04-27 20:10:36 -0300

    Uso [easy_thumbnails](https://github.com/SmileyChris/easy-thumbnails/) en vez del script que teníamos para hacer las miniaturas. Hay que seguir estos pasos de instalación:
    1- Instalar easy_thumnails:
    
        $ pip install easy-thumbnails
    
    2- Sincronizar las tablas de este:
    
        # en el dir del proyecto
        $ ./manage syncdb

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'senderonorte.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.markup',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'easy_thumbnails',
     'senderonorte',
     'excurtion',
     'news',
