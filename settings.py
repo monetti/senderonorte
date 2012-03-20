bb8ff09e93f2b73117be4197f9a80ae46ea55867
Martin Onetti <onetti.martin@gmail.com>
2012-03-20 11:33:27 -0300

    cambios en el diseño

--- settings.py
+++ settings.py
@@ -103,7 +103,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.i18n',
     'django.core.context_processors.request',
-    'senderonorte.context_processors.latest_tweets',
 )
 
 TWITTER_USER = "@senderonorteta"
